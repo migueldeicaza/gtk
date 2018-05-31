777a6883ba6d6127fab27d097248e8f152ecd45c
miguel <miguel.de.icaza@gmail.com>
2018-05-30 22:35:23 -0400

    0055-gtk-add-gtk_widget_get_scale_factor.patch

--- gtk/gtkwidget.h
+++ gtk/gtkwidget.h
@@ -1055,6 +1055,7 @@ GdkVisual*   gtk_widget_get_visual	(GtkWidget	*widget);
 
 GdkScreen *   gtk_widget_get_screen      (GtkWidget *widget);
 gboolean      gtk_widget_has_screen      (GtkWidget *widget);
+gdouble       gtk_widget_get_scale_factor (GtkWidget *widget);
 GdkDisplay *  gtk_widget_get_display     (GtkWidget *widget);
 GdkWindow *   gtk_widget_get_root_window (GtkWidget *widget);
 GtkSettings*  gtk_widget_get_settings    (GtkWidget *widget);
